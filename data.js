f7c7a72a327c89514e402c5245b6253d0eeaea12
Michael van Bale <mvanbale@gmail.com>
2016-10-10 22:27:10 +0200

    put connection info in config

--- data.js
+++ data.js
@@ -1,9 +1,15 @@
+// get our database information
+var config = require('config');
+var dbInfo = config.get('database');
+
 // initialize DB
 const crate = require('node-crate');
 module.exports.connectToDB = () =>{
-  crate.connect ('178.62.215.200', 4200);
+  crate.connect (dbInfo.ip, dbInfo.port);
 }
 
+
+// query functions
 module.exports.allPlanes = () =>{
   console.log('Querying for all planes.');
   return crate.execute("select * from planes where true").error(console.log);
@@ -18,6 +24,7 @@ module.exports.planesByType = (planeType) =>{
  console.log(`Querying for plane with the type ${planeType}.`);
   return crate.execute("select * from planes,planetypes where planetype = ? AND planes.planetype = planetypes.name", [planeType]).error(console.log);
 }
+
 module.exports.planesByName = (name) =>{
   console.log(`Querying for plane with the name ${name}.`);
   return crate.execute("select * from planes, planetypes where planes.planetype = planetypes.name = ?", [name]).error(console.log);
--- routes.js
+++ routes.js
@@ -4,24 +4,24 @@ module.exports = (rest)=>{
 rest.get('/api/planes', function(req, rest) {
     data.allPlanes().
         success(res=>rest.ok(res.json)).
-        error(error => rest.fail(error));
+        error(error => rest.internalServerError(error));
 });
 
 rest.get('/api/planes/:name', function(req, rest) {
     data.planesByName(req.params.name).
         success(res=>rest.ok(res.json)).
-        error(error => rest.fail(error));
+        error(error => rest.internalServerError(error));
 });
 
 rest.get('/api/planetypes', function(req, rest) {
     data.allPlaneTypes(req.params.type).
         success(res=>rest.ok(res.json)).
-        error(error => rest.fail(error));
+        error(error => rest.internalServerError(error));
 });
 
 rest.get('/api/planes/type/:planetype', function(req, rest) {
     data.planesByType(req.params.planetype).
         success(res=>rest.ok(res.json)).
-        error(error => rest.fail(error));
+        error(error => rest.internalServerError(error));
 });
 }
